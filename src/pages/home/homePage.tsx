abcbd41bf323e23fdad664b313014c0402906396
alexandre <alexandre.courbon@epitech.eu>
2023-07-09 11:29:20 +0200

    add Url

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,6 +1,11 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import "./homePage.css";
 import classNames from "classnames";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage, ref, listAll, getDownloadURL, StorageReference } from 'firebase/storage';
 
 // @ts-ignore
 import VideBg from "../../asset/bg.mp4";
@@ -9,6 +14,21 @@ import GlobalHeader from "../components/GlobalHeader/globalHeader";
 // @ts-ignore
 import Loading from "../components/Loading/loading";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAR6w26vFPpGtT5H2l2o1z9ngAjGjdi35U",
+  authDomain: "loguiso.firebaseapp.com",
+  projectId: "loguiso",
+  storageBucket: "loguiso.appspot.com",
+  messagingSenderId: "134169930879",
+  appId: "1:134169930879:web:20ee6d2bc68203b8e2634a",
+  measurementId: "G-NQ2B40XRHE"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
 const LogoListBand = React.lazy(
   () => import("../components/LogoListBand/logoListBand")
 );
@@ -21,28 +41,49 @@ const Work = React.lazy(() => import("../components/Work/work"));
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
 
-  const handleImageLoad = () => {
-    console.log("ici");
-    setIsLoading(true);
-  };
+  useEffect(() => {
+    const storageRef: StorageReference = ref(storage);
+    const imagesRef: StorageReference = ref(storageRef, "/");
+    listAll(imagesRef)
+      .then((res) => {
+        const downloadPromises = res.items.map((itemRef) =>
+          getDownloadURL(itemRef)
+        );
 
-  const handleImageError = () => {
-    setIsLoading(false);
-    setIsError(true);
-  };
+        Promise.all(downloadPromises)
+          .then((urls) => {
+            setImageUrls(urls);
+            setIsLoading(true);
+          })
+          .catch((error) => {
+            console.log(error);
+            setIsError(true);
+            setIsLoading(true);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+        setIsLoading(true);
+      });
+  }, []);
+
 
   return (
     <div className="flex flex-col w-full h-full bg-black scroll-smooth">
       <GlobalHeader />
       <div className="md:px-[100px] px-2">
         <video
+/*
           src={VideBg}
+*/
+          src={imageUrls[0]}
+
           preload="auto"
           autoPlay={true}
           loop
-          onLoadedData={handleImageLoad}
-          onError={handleImageError}
           muted
           className={classNames(
             "w-full h-auto max-h-[851.56px] object-cover md:rounded-[58px]",
