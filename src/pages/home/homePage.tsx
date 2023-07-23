d2c51dbb542a78c05ff4707595b253ea773d37e7
alexandre <alexandre.courbon@epitech.eu>
2023-07-23 19:47:41 +0200

    Clean code

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,11 +1,6 @@
 import React, { Suspense, useState } from "react";
 import "./homePage.css";
 import classNames from "classnames";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage } from 'firebase/storage';
 import Work from "../components/Work/work"
 
 
@@ -17,21 +12,6 @@ import GlobalHeader from "../components/GlobalHeader/globalHeader";
 import Loading from "../components/Loading/loading";
 
 
-const configValue = {
-  apiKey: process.env.REACT_APP_FIREBASE_CONFIGURATION_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_CONFIGURATION_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_CONFIGURATION_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_CONFIGURATION_STORAGE,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIGURATION_MESSAGING,
-  appId: process.env.REACT_APP_FIREBASE_CONFIGURATION_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_CONFIGURATION_MEASUREMENTID,
-}
-
-console.log(configValue)
-// Initialize Firebase
-const app = initializeApp(configValue);
-const storage = getStorage(app);
-const analytics = getAnalytics(app);
 const LogoListBand = React.lazy(
   () => import("../components/LogoListBand/logoListBand")
 );
@@ -47,40 +27,7 @@ const About = React.lazy(() => import("../components/About/about"));
 const Work = React.lazy(() => import("../components/Work/work"));
 */
 const HomePage = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [imageUrls, setImageUrls] = useState<string[]>([]);
-
-/*  useEffect(() => {
-    const storageRef: StorageReference = ref(storage);
-    const imagesRef: StorageReference = ref(storageRef, "/");
-    listAll(imagesRef)
-      .then((res) => {
-        const downloadPromises = res.items.map((itemRef) =>
-          getBlob()
-          //getURL(itemRef)
-        );
-        console.log(downloadPromises)
-
-        /!*Promise.all(downloadPromises)
-          .then((urls) => {
-            setImageUrls(urls);
-            setIsLoading(true);
-          })
-          .catch((error) => {
-            console.log(error);
-            setIsError(true);
-            setIsLoading(true);
-          });*!/
-      })
-      .catch((error) => {
-        console.log(error);
-        setIsError(true);
-        setIsLoading(true);
-      });
-  }, []);*/
-
-  console.log(imageUrls[0])
+  const [isLoading] = useState(true);
 
   return (
     <div className="flex flex-col w-full h-full bg-black scroll-smooth">
